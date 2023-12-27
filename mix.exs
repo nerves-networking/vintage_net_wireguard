bbee89e5dbd2edaa66aa803aa45b3bc81906c4f2
Jon Carstens <jjcarstens@me.com>
2023-12-26 17:48:01 -0700

    v0.2.0 release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule VintageNetWireguard.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @source_url "https://github.com/nerves-networking/vintage_net_wireguard"
 
   def project() do
